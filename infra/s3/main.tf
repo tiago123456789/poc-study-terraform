e62d6113e526d8a972a42f365597518ad08c60d6
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-02-04 19:01:28 -0300

    try resolve pipeline create or destroy s3 resources

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -13,39 +13,8 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.bucket_name}-${var.environment}" 
-
-  website {
-    index_document = "index.html"    
-    error_document = "error.html"    
-  }
 }
 
-
-resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
-  bucket = aws_s3_bucket.my_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-  bucket = aws_s3_bucket.my_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "static_site_bucket" {
-  depends_on = [
-	aws_s3_bucket_public_access_block.static_site_bucket,
-	aws_s3_bucket_ownership_controls.static_site_bucket,
-  ]
-
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "public-read"
-}
 
 
 
