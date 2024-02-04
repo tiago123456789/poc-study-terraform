b61d9597fca091230d0703e31af7dbdc2ac23c81
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-02-04 19:07:07 -0300

    changes to set envirnoment when create sqs resource

--- infra/sqs/main.tf
+++ infra/sqs/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
-  name                      = var.queue_name
+  name                      = "${var.queue_name}_${var.environment}"
   delay_seconds             = 0
   max_message_size          = 2048
   message_retention_seconds = 86400
